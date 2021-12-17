9931491ce58a1ccecde294ec85c3b50014ec7e02
UmbWill <pumberto55@gmail.com>
2021-12-17 23:55:25 +0900

    Fixed render in views...I need to sleep!

--- ublog-dj/ublog/backblog/views.py
+++ ublog-dj/ublog/backblog/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def post_list(request):
-    return resnder(request,'backblog/templates/post_list.html',{})
+    return render(request,'backblog/templates/post_list.html',{})
