ce143678eb6b17c5230c803133a5759725d1a135
UmbWill <pumberto55@gmail.com>
2021-12-19 23:35:16 +0900

    ALLOWED_HOSTS

--- ublog-dj/ublog/ublog/settings.py
+++ ublog-dj/ublog/ublog/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-m!&au@%txixw9*k5h4uzfd*_8$^n+gusi_wjv2j+n1sbvh!g35
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','umbland.com']
+ALLOWED_HOSTS = ['127.0.0.1','umbland.com','umbwill.pythonanywhere.com']
 
 
 # Application definition
